186d61a05ae29d6c9e3a404ce9152b5eeb8347d9
unknown <bruno.coelho@PC-40747.TCDF.NET>
2015-11-17 18:28:40 -0200

    Alteração local

--- ArqRemote.php
+++ ArqRemote.php
@@ -1,5 +1,11 @@
 <?php
 
 echo 'arquivo remoto';
+echo 'arquivo remoto1';
+echo 'arquivo remoto2';
+echo 'arquivo remoto3';
+echo 'arquivo remoto4';
+echo 'arquivo remoto5';
+echo 'arquivo remoto6';
 
 ?>
